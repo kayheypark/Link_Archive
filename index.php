e6c6f075bf18e8c426e6938c8ede7079ce37dc4e
kayheypark <kayheypark@gmail.com>
2022-01-04 02:07:06 +0900

    tablet responsive start

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <?php include("./view/shared/layout/head.php") ?>
 <section class="sec">
     <h2>Main</h2>
+    <p>
+        Look at your scattered SNS link on one card.<br />
+        And look at other people's cards.
+    </p>
 </section>
 <?php include("./view/shared/layout/tail.php") ?>
 <?php include("./view/shared/layout/tail.sub.php") ?>
